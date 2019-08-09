c65ebd90cd8b320e1897469eac48af4970db138f
Paul Standley <paulstandley1972@gmail.com>
2019-08-09 23:31:21 +0100

    set up toggle and delete

--- todolist/src/app/components/todo-item/todo-item.component.ts
+++ todolist/src/app/components/todo-item/todo-item.component.ts
@@ -20,4 +20,13 @@ export class TodoItemComponent implements OnInit {
    };
    return classes;
  }
+
+ onToggle(todo) {
+   console.log('toggle');
+ }
+
+ onDelete(todo) {
+  console.log('delete');
+}
+
 }
