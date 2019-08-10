0bb46e4020ae58f16da4bb5e3feb83d220b5f198
Paul Standley <paulstandley1972@gmail.com>
2019-08-10 20:35:59 +0100

    Pass Tests

--- todo-list-test/src/app/app.component.spec.ts
+++ todo-list-test/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-list-test!');
+    expect(compiled.querySelector('h1').textContent).toContain('TODO-LIST-TEST');
   });
 });
--- todo-list-test/src/app/app.component.ts
+++ todo-list-test/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-test';
+  title: string = 'todo-list-test';
 }
