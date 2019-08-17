dcc3649555388baecf2002e7b5f44911c8a1ee1e
Paul Standley <paulstandley1972@gmail.com>
2019-08-17 12:32:16 +0100

    it works but all tests failed

--- todo-list-test/src/app/services/todo.service.ts
+++ todo-list-test/src/app/services/todo.service.ts
@@ -1,29 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ];
+  constructor(private http: HttpClient) { }
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.todosUrl);
   }
 }
